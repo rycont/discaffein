445dc5f057cd6f9db190f7e34c19bc37c4941ffd
rycont <reactdev@kakao.com>
2021-01-02 21:36:57 +0900

    refactor : rename to "forwardToDiscord"

--- src/kakao/index.ts
+++ src/kakao/index.ts
@@ -2,7 +2,7 @@ import login from "./login";
 
 import * as KakaoTypes from "node-kakao";
 import { kakao } from "./";
-import forwardToDiscord from "./sendToDiscord";
+import forwardToDiscord from "./forwardToDiscord";
 
 export { default as kakao } from "../storages/kakao";
 
