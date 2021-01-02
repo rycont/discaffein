3f8a8f84483c106f3e531667f01144a09285e53c
rycont <reactdev@kakao.com>
2021-01-02 21:34:26 +0900

    feat : can see kakao reply in discord

--- src/kakao/sendToDiscord.ts
+++ src/kakao/sendToDiscord.ts
@@ -2,35 +2,49 @@ import { Chat, ChatType } from "node-kakao";
 import { KakaoAPI } from 'node-kakao/dist/kakao-api'
 import { MessageEmbed } from "discord.js";
 import * as channel from "../bridge/channelFinder";
+import deepmerge from "deepmerge";
 
 const forwardToDiscord = async (kakaoChat: Chat) => {
     const kakaoSender = kakaoChat.Channel.getUserInfo(kakaoChat.Sender)!!
     const discordChannel = await channel.k2d(kakaoChat.Channel)
+
+    if(kakaoChat.RawAttachment?.url && !kakaoChat.RawAttachment.thumbnailUrl) {
+        discordChannel.send({
+            files: [kakaoChat.RawAttachment.url]
+        })
+    }
+    const attaches = kakaoChat.AttachmentList.reduce((acc, attach) => {
+        const readableAttach = attach.toJsonAttachment()
+        if (attach.RequiredMessageType === ChatType.Sticker) {
+            return deepmerge(acc, {
+                files: [KakaoAPI.getEmoticonImageURL(readableAttach.path)]
+            })
+        }
+        if (attach.RequiredMessageType === ChatType.Photo) {
+            return deepmerge(acc, {
+                files: [readableAttach.url]
+            })
+        }
+        if(attach.RequiredMessageType === ChatType.Reply) {
+            const replyOriginSender = kakaoChat.Channel.getUserInfoId(readableAttach.src_userId)?.Nickname
+            return deepmerge(acc, {
+                footer: {
+                    iconURL: "https://firebasestorage.googleapis.com/v0/b/rycont-shared.appspot.com/o/reply.png?alt=media",
+                    text: `${replyOriginSender} : ${readableAttach.src_message}`,
+                }
+            })
+        }
+        return acc
+    }, {})
 
     discordChannel.send(new MessageEmbed({
         author: {
             name: kakaoSender.Nickname,
             iconURL: kakaoSender.FullProfileImageURL
         },
-        description: kakaoChat.Text
+        description: kakaoChat.Text,
+        ...attaches
     }))
-    if(kakaoChat.RawAttachment?.url && !kakaoChat.RawAttachment.thumbnailUrl) {
-        discordChannel.send({
-            files: [kakaoChat.RawAttachment.url]
-        })
-    }
-    kakaoChat.AttachmentList.forEach((attach) => {
-        if (attach.RequiredMessageType == ChatType.Sticker) {
-            return discordChannel.send("", {
-                files: [KakaoAPI.getEmoticonImageURL(attach.toJsonAttachment().path)]
-            })
-        }
-        if (attach.RequiredMessageType == ChatType.Photo) {
-            return discordChannel.send("", {
-                files: [attach.toJsonAttachment().url]
-            })
-        }
-    })
 }
 
 export default forwardToDiscord
